e101bb922e4d2da875b92062af867b7378d8ad5c
Alec Thomas <alec@swapoff.org>
2014-04-02 10:59:38 -0400

    Compatibility: commandLine -> CommandLine

--- export_test.go
+++ export_test.go
@@ -15,7 +15,7 @@ import (
 // After calling ResetForTesting, parse errors in flag handling will not
 // exit the program.
 func ResetForTesting(usage func()) {
-	commandLine = &FlagSet{
+	CommandLine = &FlagSet{
 		name:          os.Args[0],
 		errorHandling: ContinueOnError,
 		output:        ioutil.Discard,
@@ -24,6 +24,6 @@ func ResetForTesting(usage func()) {
 }
 
 // CommandLine returns the default FlagSet.
-func CommandLine() *FlagSet {
-	return commandLine
+func GetCommandLine() *FlagSet {
+	return CommandLine
 }
